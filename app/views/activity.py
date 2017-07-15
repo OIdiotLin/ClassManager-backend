9c0aa4889bfb09c97e5c8eed9130a7692bc58544
H.D. Lin <OIdiotLin@users.noreply.github.com>
2017-07-15 17:49:48 +0800

    sync with person table when API - delete_activity invoked

--- app/views/activity.py
+++ app/views/activity.py
@@ -102,6 +102,14 @@ def delete_activity(request):
 						'err_info': 'no object has id = ' + request.POST.get('id')
 					}
 				)
+
+			participators = target_activity.first().participator.split(',')
+
+			for stu_num in participators:
+				person = Person.objects.get(student_number = stu_num)
+				person.participation -= int(target_activity.first().participation)
+				person.save()
+
 			target_activity.delete()
 			return JsonResponse({'status': 'success'})
 
